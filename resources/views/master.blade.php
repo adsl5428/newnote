523576c99f5d34d0434a25a2289f65e4e108acf2
lihongcheng <1519492056@qq.com>
2017-04-23 15:39:56 +0800

    image2

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -95,20 +95,20 @@
         <div class="weui-tabbar">
             {{--<a href="javascript:;" class="weui-tabbar__item weui-bar__item_on">--}}
             {{--<span style="display: inline-block;position: relative;">--}}
-            {{--<img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">--}}
+            {{--<img src="./public/images/icon_tabbar.png" alt="" class="weui-tabbar__icon">--}}
             {{--<span class="weui-badge" style="position: absolute;top: -2px;right: -13px;">8</span>--}}
             {{--</span>--}}
             {{--<p class="weui-tabbar__label">微信</p>--}}
             {{--</a>--}}
             <a href="javascript:;" class="weui-tabbar__item">
-                <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
+                <img src="./public/images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
                 <p class="weui-tabbar__label">首页</p>
             </a>
 
             @if( Session::get('name')==null)
                 <a href="javascript:;" class="weui-tabbar__item">
                 <span style="display: inline-block;position: relative;">
-                    <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
+                    <img src="./public/images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
                     <span class="weui-badge weui-badge_dot" style="position: absolute;top: 0;right: -6px;"></span>
                 </span>
                     <p class="weui-tabbar__label">登录</p>
@@ -116,13 +116,13 @@
             @else
                 <a href="javascript:;" class="weui-tabbar__item">
                 <span style="display: inline-block;position: relative;">
-                    <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
+                    <img src=".public/images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
                     <span class="weui-badge weui-badge_dot" style="position: absolute;top: 0;right: -6px;"></span>
                 </span>
                     <p class="weui-tabbar__label">我</p>
                 </a>
                 <a href="javascript:;" class="weui-tabbar__item">
-                    <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
+                    <img src="./public/images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
                     <p class="weui-tabbar__label">发表</p>
                 </a>
             @endif
